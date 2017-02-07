93619b8c3e2476064bd134a7bcf6a0cc48fb5742
yotion <yotionking@163.com>
2017-02-07 20:56:27 +0800

    Initial commit

---
+++ src/com/yotionking/util/Test.java
@@ -0,0 +1,10 @@
+package com.yotionking.util;
+
+/**
+ * Created by kurt on 2017/2/7.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello Intellij");
+    }
+}
