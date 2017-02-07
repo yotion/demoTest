fdae297225940f870dd7d7ca42296f1335fedc3c
yotion <yotionking@163.com>
2017-02-07 21:32:54 +0800

    this is first commit

--- src/com/yotionking/util/Test.java
+++ src/com/yotionking/util/Test.java
@@ -6,5 +6,7 @@ package com.yotionking.util;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello Intellij");
+        String str = "123";
+        System.out.println(str);
     }
 }
